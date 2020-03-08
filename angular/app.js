6cda10faa3b26cb004c07ac80d15a405f41a3251
JanezTheProgramer <brina.jehart@gmail.com>
2020-03-08 22:01:39 +0100

    design updates lol

--- angular/app.js
+++ angular/app.js
@@ -1,9 +1,10 @@
-angular.module('ngView', [], function ($routeProvider, $locationProvider) {
-    
+var app = angular.module('ngView', [], function ($routeProvider, $locationProvider) {
+
+
     $routeProvider
-        .when('/', { templateUrl: "./views/default.html" })
-        .when('/login', { templateUrl: "./views/login.html" })
-        .when('/register', { templateUrl: "./views/register.html" })
+        .when('/', { templateUrl: "./views/default.html", controller: "DefController" })
+        .when('/login', { templateUrl: "./views/login.html", controller: "LoginController" })
+        .when('/register', { templateUrl: "./views/register.html", controller: "RegisterController" })
         .otherwise({ redirectTo: "/login" });
 
     $locationProvider.html5Mode(false);
@@ -17,3 +18,13 @@ function MainCntl($scope, $route, $routeParams, $location, $window) {
         $window.history.back();
     }
 }
+
+app.controller('DefController', function ($scope) {
+    $scope.name = 'def';
+});
+app.controller('LoginController', function ($scope) {
+    $scope.name = 'login';
+});
+app.controller('RegisterController', function ($scope) {
+    $scope.name = 'register';
+});
